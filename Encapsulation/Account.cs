ac100d01c31cc6fd47e0800df4622e9b9898c7cf
“mayuri” <mayurisalunkhe01@gmail.com>
2021-10-07 19:12:51 +0530

    adding concept Encapsulation

---
+++ Encapsulation/Account.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learning_oops_concept.Encapsulation
+{
+    class Account
+    {
+        int accountBalance = 1000;
+
+        public void SetBalance(int amount)
+        {
+            if(amount<0)
+            {
+                Console.WriteLine("you can not pass negative value");
+            }
+            else
+            {
+                accountBalance = amount;
+            }
+ 
+        }
+        public void GetBalance()
+        {
+            Console.WriteLine("Your Account Balance is :" + accountBalance);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Account myAccount = new Account();
+            myAccount.SetBalance(-20);
+            myAccount.GetBalance();
+            Console.ReadLine();
+        }
+    }
+
+
+         
+}
