8ee0d9750c0bfdd302d74bc7bf5585cac4cc2dae
“mayuri” <mayurisalunkhe01@gmail.com>
2021-10-09 07:36:16 +0530

    Polymorphism

---
+++ Polymorphism/methodOveriding/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace MethodOverriding
+{
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Animal is eating");
+        }
+    }
+    class Dog : Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("Dog is eating");
+        }
+
+        public static void Main(string[] args)
+        {
+            Dog leo = new Dog();
+            leo.Eat();
+            Console.ReadLine();
+        } 
+    }
+}
