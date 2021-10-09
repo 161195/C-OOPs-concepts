e5fb84feb918db0e2c733a8edca7d8c30bc3aadd
“mayuri” <mayurisalunkhe01@gmail.com>
2021-10-09 16:46:28 +0530

    adding polymorphism concept

---
+++ Polymorphism/methodOverloading/program/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace methodOverloading
+{
+    class Program
+    {
+        void Sum(int a, int b)
+        {
+            Console.WriteLine(a + b);
+        }
+        void Sum(float a, float b)
+        {
+            Console.WriteLine(a + b);
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.Sum(40.5f, 41.5f);
+            Console.ReadLine();
+        }
+    }
+}
